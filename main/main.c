c8dbc61c81fc8f65ae7c510c14614b123b27983c
Osama Salah-ud-Din <osama.salahuddin@gmail.com>
2025-12-10 20:28:47 +0100

    Using Both Wifi and BT together

---
+++ main/main.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+
+void bt_main(void);
+void ap_main(void);
+
+void app_main(void)
+{
+
+    /* initialize NVS — it is used to store PHY calibration data */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    /* Start the SoftAP */
+    ap_main();
+
+    /* Start the Bluetooth A2DP Sink */
+    bt_main();
+}
